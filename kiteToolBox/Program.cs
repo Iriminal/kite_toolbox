7627dc2b09b7a903cd46b31504d145639021d8df
Iriminal <1131288411@qq.com>
2016-07-27 11:33:10 +0800

    Edit Program.cs
    + Add Network Exception Detector

--- kiteToolBox/Program.cs
+++ kiteToolBox/Program.cs
@@ -21,17 +21,25 @@ namespace kiteToolBox
             String productVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
             //Request update
-            var client = new WebClient();
-            String s = client.DownloadString("http://api-acecyber.rhcloud.com/hasUpdate?currentVersion=" + productVersion);
+            try
+            {
 
-            s = s.Replace("<html>", "");
-            s = s.Replace("</html>", "");
+                var client = new WebClient();
+                String s = client.DownloadString("http://api-acecyber.rhcloud.com/hasUpdate?currentVersion=" + productVersion);
+                s = s.Replace("<html>", "");
+                s = s.Replace("</html>", "");
+
+                int i = s.IndexOf(',');
+                String ver = s.Substring(0, i);
+                if (ver == "0") goto a;
+                String info = s.Substring(i + 1, s.Length - i - 1);
+                Application.Run(new NewVersion(ver, info));
+            }
+            catch
+            {
+                return;
+            }
   
-            int i = s.IndexOf(',');
-            String ver = s.Substring(0,i);
-            if (ver == "0") goto a;
-            String info = s.Substring(i + 1, s.Length - i - 1);
-            Application.Run(new NewVersion(ver, info));
             return;
 
             a:
